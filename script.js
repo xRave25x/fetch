607608c1cccce28dc66db204adbbd2769d6b5b9b
Andrei Demichev <andyuson24@gmail.com>
2024-01-31 21:15:35 +0200

    выполнил получение и отправку данных по заданию 26

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'
+const urlGet = 'db.json';
+const urlSend = 'https://jsonplaceholder.typicode.com/posts';
+
+const getData = (url) => { 
+    return fetch(url)
+    .then(response => response.json())
+    .catch(error => {
+        console.log(error);
+    })
+};
+
+
+const sendData = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+    .then(response => response.json())
+};
+
+
+getData(urlGet);
+sendData(urlSend);
